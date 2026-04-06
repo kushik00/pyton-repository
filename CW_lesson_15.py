a63497660f734384af5872f239fd8dcf1c8c0e3a
Lada Shcherbakova <146574580+kushik00@users.noreply.github.com>
2026-04-06 08:42:44 +0300

    Create CW_lesson_15.py

---
+++ CW_lesson_15.py
@@ -0,0 +1,48 @@
+import os
+import cv2
+import time
+from ultralytics import YOLO
+
+PROJECT_DIR = os.path.dirname(__file__)
+VIDEO_DIR = os.path.join(PROJECT_DIR, 'video')
+OUT_DIR = os.path.join(PROJECT_DIR, 'out')
+
+os.makedirs(OUT_DIR, exist_ok=True)
+
+USE_WEBCAM = True
+
+if USE_WEBCAM:
+    cap = cv2.VideoCapture(0)
+else:
+    VIDEO_PATH = os.path.join(VIDEO_DIR, 'name')
+    cap = cv2.VideoCapture(VIDEO_PATH)
+
+model = YOLO('yolov8s.pt')
+
+CONF_THRESHOLD =0.5
+RESIZE_WIDTH = 960
+
+prev_time = time.time()
+fps = 0.0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    if RESIZE_WIDTH is not None:
+        h, w = frame.shape[:2]
+        scale = RESIZE_WIDTH / w
+        new_w = RESIZE_WIDTH
+        new_h = int(scale * h)
+
+        frame = cv2.resize(frame, (new_w, new_h))
+
+
+    result = model(frame, conf_thresh=CONF_THRESHOLD, verbose=False)
+
+    people_count = 0
+    psevdo_id = 0
+
+    PERSON_CLASS_ID = 0
+    
